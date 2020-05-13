04a9dbb0624a66c9a08d53a521fc44bbd1311c16
wanqx <995293737@qq.com>
2020-05-13 16:44:29 +0800

    add time wait

--- launch.sh
+++ launch.sh
@@ -6,7 +6,8 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
+sleep 0.5
 
 # Launch bar1 and bar2
 polybar top -c ~/.config/polybar/config-top.ini &
